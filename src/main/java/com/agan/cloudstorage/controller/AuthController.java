7a19cca97a6dca4f439bdd5ddcaf8aa125b5e76a
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-07-03 23:45:24 +0300

    AuthController uptaded. RequestMapping changed

--- src/main/java/com/agan/cloudstorage/controller/AuthController.java
+++ src/main/java/com/agan/cloudstorage/controller/AuthController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("/auth")
+@RequestMapping("/")
 public class AuthController {
 
     private final AuthenticationManager authenticationManager;

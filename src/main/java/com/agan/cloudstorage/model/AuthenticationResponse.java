0c95fe4ec082754f1ea90ef99661af5e32dcb868
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-07-04 22:37:40 +0300

    AuthenticationResponse corrected with auth-token

--- src/main/java/com/agan/cloudstorage/model/AuthenticationResponse.java
+++ src/main/java/com/agan/cloudstorage/model/AuthenticationResponse.java
@@ -1,10 +1,12 @@
 package com.agan.cloudstorage.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class AuthenticationResponse {
+    @JsonProperty("auth-token")
     private String jwt;
 }

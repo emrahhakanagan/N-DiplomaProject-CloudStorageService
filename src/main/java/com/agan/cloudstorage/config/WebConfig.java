526643ad04cc3420b3589987990de88a9eefc71e
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-07-05 14:45:55 +0300

    trying of resolving problem with login form

--- src/main/java/com/agan/cloudstorage/config/WebConfig.java
+++ src/main/java/com/agan/cloudstorage/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8081")
+                .allowedOrigins("http://localhost:8085")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);

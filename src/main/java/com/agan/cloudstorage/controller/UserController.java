d7ddd366e571fe3f19ebc934f77521746cc12d61
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-09-16 22:20:08 +0300

    feat(controller): Added UserController for user creation

---
+++ src/main/java/com/agan/cloudstorage/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.agan.cloudstorage.controller;
+
+import com.agan.cloudstorage.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/addUser")
+    public ResponseEntity<String> addUser(@RequestBody User user) {
+        userService.addUser(user);
+        return ResponseEntity.ok("User added successfully");
+    }
+}
+

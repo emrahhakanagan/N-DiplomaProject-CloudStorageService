d1ab5315601bb5d4d350299e81aa6e6b7b2cf517
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-09-17 15:17:52 +0300

    feat(service): Implemented user addition with validation and password encryption

---
+++ src/main/java/com/agan/cloudstorage/service/UserService.java
@@ -0,0 +1,31 @@
+package com.agan.cloudstorage.service;
+
+import com.agan.cloudstorage.model.User;
+import com.agan.cloudstorage.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+
+    public void addUser(User user) {
+
+        if (userRepository.findByUsername(user.getUsername()).isPresent()) {
+            throw new IllegalArgumentException("A user with this username already exists");
+        }
+
+        if (user.getPassword() == null || user.getPassword().length() < 6) {
+            throw new IllegalArgumentException("Password must contain at least 6 characters");
+        }
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        userRepository.save(user);
+    }
+}
+

bcf50495a1051b8b4cb7198f139536a4473ae8d7
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-09-26 23:29:14 +0300

    feat: Added exception handling for missing file in listFiles endpoint

---
+++ src/main/java/com/agan/cloudstorage/exception/FilesNotFoundException.java
@@ -0,0 +1,8 @@
+package com.agan.cloudstorage.exception;
+
+public class FilesNotFoundException extends RuntimeException {
+    public FilesNotFoundException(String message) {
+        super(message);
+    }
+}
+

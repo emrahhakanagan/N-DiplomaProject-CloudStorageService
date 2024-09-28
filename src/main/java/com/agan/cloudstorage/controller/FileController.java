ae3a6389f785e083da8cde5bd39f9fca3071bcf1
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-09-28 16:39:26 +0300

    refactor: Removed unused uploadFile method and added editFileName endpoint with requestBody validation and auth-token handling

--- src/main/java/com/agan/cloudstorage/controller/FileController.java
+++ src/main/java/com/agan/cloudstorage/controller/FileController.java
@@ -8,20 +8,23 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @AllArgsConstructor
 public class FileController {
+/*
+* Getting the auth-token from the header is required by the specification, but we do not use this variable directly.
+* The authentication process is automatically handled by Spring Security through JwtRequestFilter.
+*/
 
     private final FileService fileService;
 
     @GetMapping("/list")
     public ResponseEntity<List<File>> listFiles(
-            @RequestHeader(value = "auth-token", required = true) String authToken, // Required by specification; token is processed in JwtRequestFilter
+            @RequestHeader(value = "auth-token", required = true) String authToken,
             @RequestParam(value = "limit", defaultValue = "10", required = false) int limit) {
 
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
@@ -31,13 +34,22 @@ public class FileController {
         return new ResponseEntity<>(files, HttpStatus.OK);
     }
 
+    @PutMapping("/file")
+    public ResponseEntity<?> editFileName(
+            @RequestHeader(value = "auth-token", required = true) String authToken,
+            @RequestParam("filename") String filename,
+            @RequestBody Map<String, String> requestBody) {
 
-    @PostMapping("/file")
-    public ResponseEntity<File> uploadFile(@RequestParam("file") MultipartFile file,
-                                           @RequestParam String userId) throws IOException {
-        File uploadedFile = fileService.uploadFile(file, userId);
-        return new ResponseEntity<>(uploadedFile, HttpStatus.OK);
+        String newFileName = requestBody.get("name");
+
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        fileService.editFileName(username, filename, newFileName);
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
+
+
 
     @GetMapping("/file")
     public ResponseEntity<byte[]> downloadFile(@RequestParam String fileId, @RequestParam String userId) {

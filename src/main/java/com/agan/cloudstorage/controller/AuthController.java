99eed388962728a6e091641aa7a303b6a1e57c51
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-09-29 19:41:27 +0300

    refactor: Updated AuthController - streamline login and logout methods, improve JWT handling, and ensure specification compliance

--- src/main/java/com/agan/cloudstorage/controller/AuthController.java
+++ src/main/java/com/agan/cloudstorage/controller/AuthController.java
@@ -2,19 +2,9 @@ package com.agan.cloudstorage.controller;
 
 import com.agan.cloudstorage.model.AuthenticationRequest;
 import com.agan.cloudstorage.model.AuthenticationResponse;
-import com.agan.cloudstorage.service.MyUserDetailsService;
-import com.agan.cloudstorage.util.JwtUtil;
-import jakarta.servlet.http.HttpServletRequest;
+import com.agan.cloudstorage.service.AuthenticationService;
 import lombok.AllArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -28,46 +18,18 @@ public class AuthController {
      * authToken The authentication token provided in the request header.
      */
 
-    private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
-    private final AuthenticationManager authenticationManager;
-    private final MyUserDetailsService userDetailsService;
-    private final JwtUtil jwtUtil;
-    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationService authenticationService;
 
     @PostMapping("/login")
-    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest)
-            throws Exception {
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) {
 
-        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getLogin());
-        logger.info("Stored password hash for user {}: {}", authenticationRequest.getLogin(), userDetails.getPassword());
+        String jwt = authenticationService.authenticate(
+                authenticationRequest.getLogin(), authenticationRequest.getPassword()
+        );
 
-        boolean passwordMatches = passwordEncoder.matches(authenticationRequest.getPassword(), userDetails.getPassword());
-
-        // -------------- Log --------------
-        if (passwordMatches) {
-            logger.info("Password matches for user: {}", authenticationRequest.getLogin());
-        } else {
-            logger.error("Password does not match for user: {}", authenticationRequest.getLogin());
-        }
-        // -------------- Log --------------
-
-        try {
-            authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(authenticationRequest.getLogin(),
-                            authenticationRequest.getPassword())
-            );
-            logger.info("Authentication successful for user: {}", authenticationRequest.getLogin());
-        } catch (BadCredentialsException e) {
-            logger.error("Authentication failed for user: {}", authenticationRequest.getLogin());
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
-        }
-
-        logger.info("Stored password hash for user {}: {}", authenticationRequest.getLogin(), userDetails.getPassword());
-
-        final String jwt = jwtUtil.generateToken(userDetails);
-        logger.info("JWT created for user: {}", authenticationRequest.getLogin());
-
-        return ResponseEntity.ok(new AuthenticationResponse(jwt));
+        return ResponseEntity.ok()
+                .header("auth-token", jwt)
+                .body(new AuthenticationResponse(jwt));
     }
 
     @PostMapping("/logout")
@@ -75,6 +37,4 @@ public class AuthController {
 
         return ResponseEntity.ok("Successfully logged out");
     }
-
-
 }

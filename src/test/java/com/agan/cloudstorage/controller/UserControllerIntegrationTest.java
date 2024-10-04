ebdeb601a01afbfbfbb4f64673cdca550f88091d
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-10-04 21:57:17 +0300

    test(integration): Added integration tests for UserController using Testcontainers with MongoDB setup

--- src/test/java/com/agan/cloudstorage/controller/UserControllerIntegrationTest.java
+++ src/test/java/com/agan/cloudstorage/controller/UserControllerIntegrationTest.java
@@ -1,14 +1,13 @@
 package com.agan.cloudstorage.controller;
 
 import com.agan.cloudstorage.model.User;
-import com.agan.cloudstorage.repository.UserRepository;
 import com.agan.cloudstorage.service.UserService;
+import lombok.AllArgsConstructor;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,34 +18,26 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@AllArgsConstructor
 @Testcontainers
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
 public class UserControllerIntegrationTest {
 
-    // Создаем контейнер MongoDB
     static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:latest")
             .withExposedPorts(27017);
 
-    @Autowired
     private UserService userService;
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
     private UserController userController;
 
     @BeforeAll
     static void setUp() {
-        // Запускаем контейнер и устанавливаем URL для подключения к MongoDB
         mongoDBContainer.start();
         System.setProperty("spring.data.mongodb.uri", mongoDBContainer.getReplicaSetUrl());
     }
 
     @AfterAll
     static void tearDown() {
-        // Останавливаем контейнер после выполнения всех тестов
         mongoDBContainer.stop();
     }
 
@@ -70,10 +61,8 @@ public class UserControllerIntegrationTest {
         user.setUsername("existingUser");
         user.setPassword("password123");
 
-        // Добавляем пользователя в репозиторий
         userService.addUser(user);
 
-        // Проверяем, что исключение будет выброшено при повторной попытке добавить того же пользователя
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
             userController.addUser(user);
         });
